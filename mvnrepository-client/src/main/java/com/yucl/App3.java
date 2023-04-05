ef45a0ab5e286e66173260a527f5ef6933a97a1f
yuchunlei <147286318@qq.com>
2023-04-05 22:38:15 +0800

    update cdt pom.xml

--- mvnrepository-client/src/main/java/com/yucl/App3.java
+++ mvnrepository-client/src/main/java/com/yucl/App3.java
@@ -3,6 +3,6 @@ package com.yucl;
 public class App3 {
     public static void main(String[] args) {
         org.owasp.dependencycheck.App app = new org.owasp.dependencycheck.App();
-        app.run(new String[]{"-s", "L:\\workspaces\\IdeaProjects\\learn-demo", "-f", "HTML","-d","L:\\workspaces\\IdeaProjects\\dependency-check-8.2.1-release\\dependency-check\\data"});
+        app.run(new String[]{"-s", "L:\\workspaces\\IdeaProjects\\archguard", "-f", "HTML","-d","L:\\workspaces\\IdeaProjects\\dependency-check-8.2.1-release\\dependency-check\\data"});
     }
 }
