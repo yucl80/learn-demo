9a32b708dd41698e713a8ca58e311591a7fc4e06
yuchunlei <147286318@qq.com>
2023-04-08 09:43:38 +0800

    update code

--- mvnrepository-client/src/main/java/com/yucl/App3.java
+++ mvnrepository-client/src/main/java/com/yucl/App3.java
@@ -2,7 +2,11 @@ package com.yucl;
 
 public class App3 {
     public static void main(String[] args) {
+        System.setProperty("http.proxyHost", "127.0.0.1");
+        System.setProperty("http.proxyPort", "51943");
+        System.setProperty("https.proxyHost", "127.0.0.1");
+        System.setProperty("https.proxyPort", "51943");
         org.owasp.dependencycheck.App app = new org.owasp.dependencycheck.App();
-        app.run(new String[]{"-s", "L:\\workspaces\\IdeaProjects\\archguard", "-f", "HTML","-d","L:\\workspaces\\IdeaProjects\\dependency-check-8.2.1-release\\dependency-check\\data"});
+        app.run(new String[]{"-s", "L:\\workspaces\\IdeaProjects\\learn-demo", "--advancedHelp", "-f", "HTML", "-d", "L:\\workspaces\\IdeaProjects\\dependency-check-8.2.1-release\\dependency-check\\data"});
     }
 }
